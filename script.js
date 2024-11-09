bdd7e06d517ce307733cafddd330bd97ec2baf6c
jasvinderkhera <ankitkhera15@gmail.com>
2024-11-09 23:03:36 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+$('.slider').slick({
+    draggable: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    dots: true,
+    fade: true,
+    speed: 500,
+    infinite: true,
+    cssEase: 'ease-in-out',
+    touchThreshold: 100
+  })
